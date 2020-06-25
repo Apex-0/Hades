605a2db43ace048a849b442e9bd88e3f3e47dbe5
Johannes <35636315+LIWKK@users.noreply.github.com>
2020-06-26 00:35:07 +0300

    New Reach check

--- src/main/java/me/apex/hades/check/impl/combat/reach/ReachA.java
+++ src/main/java/me/apex/hades/check/impl/combat/reach/ReachA.java
@@ -7,12 +7,16 @@ import me.apex.hades.check.CheckInfo;
 import me.apex.hades.event.impl.packetevents.AttackEvent;
 import me.apex.hades.event.impl.packetevents.FlyingEvent;
 import me.apex.hades.user.User;
+import me.apex.hades.user.UserManager;
+import me.apex.hades.util.ReachUtil;
 import me.apex.hades.util.boundingbox.AABB;
 import me.apex.hades.util.boundingbox.Ray;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 @CheckInfo(name = "Reach", type = "A")
 public class ReachA extends Check {
@@ -24,7 +28,10 @@ public class ReachA extends Check {
 
     boolean attacked = false;
     Entity entity;
-    List<Double> reachs = new ArrayList<>();
+
+    //idea from sim0n
+    //improved by LIWK / Johannes
+    //Raytrace by Tecnio / undersquire
 
     @Override
     public void onHandle(PacketEvent e, User user) {
@@ -32,28 +39,26 @@ public class ReachA extends Check {
             attacked = true;
             entity = ((AttackEvent) e).getEntity();
         } else if (e instanceof FlyingEvent) {
-            if (reachs.size() >= 10) {
-                reachs.remove(0);
-            }
             if (attacked) {
                 attacked = false;
                 Ray ray = Ray.from(user);
                 double dist = AABB.from(entity).collidesD(ray, 0, 10);
-                if (dist != -1) {
-                    reachs.add(dist);
+                if (dist == -1) {
+                    return;
                 }
+                ReachUtil currentLocation = user.getReachLoc();
+                ReachUtil previousLocation = user.getLastReachLoc();
 
-                if (reachs.size() == 10) {
-                    double total = 0;
-                    double avgReach = 0;
-                    for (int i = 0; i < reachs.size(); i++) {
-                        total += reachs.get(i);
-                        avgReach = total / reachs.size();
-                    }
+                double range = UserManager.getUser((Player) entity).reachQueue.stream().mapToDouble(loc -> {
+                    double distanceX = Math.min(Math.min(Math.abs(currentLocation.getX() - loc.getMinX()), Math.abs(currentLocation.getX() - loc.getMaxX())), Math.min(Math.abs(previousLocation.getX() - loc.getMinX()), Math.abs(previousLocation.getX() - loc.getMaxX())));
+                    double distanceZ = Math.min(Math.min(Math.abs(currentLocation.getZ() - loc.getMinZ()), Math.abs(currentLocation.getZ() - loc.getMaxZ())), Math.min(Math.abs(previousLocation.getZ() - loc.getMinZ()), Math.abs(previousLocation.getZ() - loc.getMaxZ())));
 
-                    if (avgReach >= HadesPlugin.getInstance().getConfig().getDouble("Max-Reach") && dist > 2.9) {
+                    return Math.sqrt(distanceX * distanceX + distanceZ * distanceZ);
+                }).min().orElse(0.0);
+
+                    if (range >= HadesPlugin.getInstance().getConfig().getDouble("Max-Reach") && dist > 2.9) {
                         if (++preVL > 1) {
-                            flag(user, "hitting farther than possbile. r: " + avgReach + ", d: " + dist);
+                            flag(user, "hitting farther than possbile. r: " + range + ", d: " + dist);
                         }
                     } else preVL = 0;
                 }
