5245495a7d291e8fa66647d61e1e5cae2d9e5c56
undersquire <apexdev23@gmail.com>
2020-06-25 15:06:04 -0700

    Update ReachA.java

--- src/main/java/me/apex/hades/check/impl/combat/reach/ReachA.java
+++ src/main/java/me/apex/hades/check/impl/combat/reach/ReachA.java
@@ -4,19 +4,13 @@ import io.github.retrooper.packetevents.event.PacketEvent;
 import me.apex.hades.HadesPlugin;
 import me.apex.hades.check.Check;
 import me.apex.hades.check.CheckInfo;
-import me.apex.hades.event.impl.packetevents.AttackEvent;
 import me.apex.hades.event.impl.packetevents.FlyingEvent;
 import me.apex.hades.user.User;
 import me.apex.hades.user.UserManager;
 import me.apex.hades.util.ReachUtil;
 import me.apex.hades.util.boundingbox.AABB;
 import me.apex.hades.util.boundingbox.Ray;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 
 @CheckInfo(name = "Reach", type = "A")
 public class ReachA extends Check {
@@ -34,10 +28,10 @@ public class ReachA extends Check {
     public void onHandle(PacketEvent e, User user) {
         if (e instanceof FlyingEvent) {
             if(System.currentTimeMillis() - user.getLastAttackPacket() < 3L &&
-            user.getLastTarget() != null) {
+                    user.getLastTarget() != null) {
 
                 Player target = user.getLastTarget();
-                if(UserManager.getUser(target).getLastReachLoc() != null) {
+                if(user.getReachLoc() != null && user.getLastReachLoc() != null) {
 
                     Ray ray = Ray.from(user);
                     double dist = AABB.from(target).collidesD(ray, 0, 10);
