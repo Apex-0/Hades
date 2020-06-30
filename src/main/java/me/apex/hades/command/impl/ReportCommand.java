85e97e6fba8e7c05d67f7225317115fecb5c5442
undersquire <apexdev23@gmail.com>
2020-06-30 10:01:07 -0700

    Update ReportCommand.java

--- src/main/java/me/apex/hades/command/impl/ReportCommand.java
+++ src/main/java/me/apex/hades/command/impl/ReportCommand.java
@@ -15,51 +15,50 @@ import java.util.Arrays;
 public class ReportCommand extends CommandAdapter {
     @Override
     public boolean onCommand(Player player, UserInput input) {
-        if (!HadesConfig.REPORT_ENABLED)return false;
+        if (!HadesConfig.REPORT_ENABLED) return false;
         User user = UserManager.getUser(player);
-         if (user.getLastReport() == null || (user.getLastReport() - System.currentTimeMillis()) >= 60000 ){
-             if (input.label().equalsIgnoreCase("report")){
-                 if (input.args().length > 0){
-                     String[] modifiedArray = Arrays.copyOfRange(input.args(), 1, input.args().length);
-                     String reason = String.join(" ", modifiedArray);
-                     Player reportedPlayer = Bukkit.getPlayer(input.args()[0]);
-                     if (reason.length() > 0) {
-                         if (reportedPlayer != null){
-                             User reportedUser = UserManager.getUser(reportedPlayer);
-                             if (reportedUser != null){
-                                 reportedUser.setReports(reportedUser.getReports() + 1);
-                                 ChatUtil.sendMsgToStaff(" ");
-                                 ChatUtil.sendMsgToStaff(HadesConfig.BASE_MESSAGE_COLOR + "Player: " + reportedPlayer.getName() + " has been reported for " + reason + "!");
-                                 ChatUtil.sendMsgToStaff(" ");
-                                 user.setLastReport(System.currentTimeMillis());
-                             }
-                         }else{
-                             user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Player does not exist!");
-                         }
-                     }else{
-                         if (reportedPlayer != null){
-                             User reportedUser = UserManager.getUser(reportedPlayer);
-                             if (reportedUser != null){
-                                 reportedUser.setReports(reportedUser.getReports() + 1);
-                                 ChatUtil.sendMsgToStaff(" ");
-                                 ChatUtil.sendMsgToStaff(HadesConfig.BASE_MESSAGE_COLOR + "Player: " + reportedPlayer.getName() + " has been reported for " + reason + "!");
-                                 ChatUtil.sendMsgToStaff(" ");
-                                 user.setLastReport(System.currentTimeMillis());
-                             }
-                         }else{
-                             user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Player does not exist!");
-                         }
-                     }
-                 }else{
-                     user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Usage: /report <player> <reason>");
-                 }
-                 return true;
-             }
+        if (input.label().equalsIgnoreCase("report")) {
+            if (user.getLastReport() == null || (user.getLastReport() - System.currentTimeMillis()) >= 60000) {
+                if (input.args().length > 0) {
+                    String[] modifiedArray = Arrays.copyOfRange(input.args(), 1, input.args().length);
+                    String reason = String.join(" ", modifiedArray);
+                    Player reportedPlayer = Bukkit.getPlayer(input.args()[0]);
+                    if (reason.length() > 0) {
+                        if (reportedPlayer != null) {
+                            User reportedUser = UserManager.getUser(reportedPlayer);
+                            if (reportedUser != null) {
+                                reportedUser.setReports(reportedUser.getReports() + 1);
+                                ChatUtil.sendMsgToStaff(" ");
+                                ChatUtil.sendMsgToStaff(HadesConfig.BASE_MESSAGE_COLOR + "Player: " + reportedPlayer.getName() + " has been reported for " + reason + "!");
+                                ChatUtil.sendMsgToStaff(" ");
+                                user.setLastReport(System.currentTimeMillis());
+                            }
+                        } else {
+                            user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Player does not exist!");
+                        }
+                    } else {
+                        if (reportedPlayer != null) {
+                            User reportedUser = UserManager.getUser(reportedPlayer);
+                            if (reportedUser != null) {
+                                reportedUser.setReports(reportedUser.getReports() + 1);
+                                ChatUtil.sendMsgToStaff(" ");
+                                ChatUtil.sendMsgToStaff(HadesConfig.BASE_MESSAGE_COLOR + "Player: " + reportedPlayer.getName() + " has been reported for " + reason + "!");
+                                ChatUtil.sendMsgToStaff(" ");
+                                user.setLastReport(System.currentTimeMillis());
+                            }
+                        } else {
+                            user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Player does not exist!");
+                        }
+                    }
+                } else {
+                    user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "Usage: /report <player> <reason>");
+                }
+            } else {
+                user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "To report again you need to wait for 1 minute!");
+            }
 
-             return false;
-        }else{
-             user.sendMessage(HadesConfig.BASE_MESSAGE_COLOR + "To report again you need to wait for 1 minute!");
-             return true;
-         }
+            return true;
+        }
+        return false;
     }
 }
